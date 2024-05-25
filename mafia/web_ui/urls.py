d637d8fcfc0ad67eb2b1557cbf82948537c364d8
Francois Volschenk <francoisv934@gmail.com>
2024-05-25 21:15:55 +0200

    Database now auto-populates the roles. Exception handling was implemented for searching the database.

--- mafia/web_ui/urls.py
+++ mafia/web_ui/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('game/<str:game_code>/start/', views.start_game, name='start_game'),
     path('api/game/<str:game_code>/players/', views.get_players, name='get_players'),
     path('api/game/role', views.get_role, name='get_role'),
-    path('done/', views.reset, name="reset")
+    path('done/<str:game_code>/', views.reset, name="reset")
 ]
